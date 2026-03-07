92b325af515cf13363f2371589de0efc27b44d20
Snehal-londhe111 <snehallondhe111@gmail.com>
2026-03-07 11:25:22 +0530

    Add book Successfully

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    Bookname = models.CharField(max_length=100)
+    Bookauthor = models.CharField(max_length=100)
+    Bookpage = models.BigIntegerField()
+    Bookprice = models.IntegerField()
+
+    class Meta:
+        db_table = 'Book'
--- bookapp/views.py
+++ bookapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
 def InsertView(request):
     return render(request, "bookapp/insert.html")
+
+def InsertData(request):
+    bn = request.POST['bname']
+    ba = request.POST['bauthor']
+    bpge = request.POST['bpage']
+    bpz = request.POST['bprice']
+
+    all_book = Book.objects.create( Bookname = bn,
+                                   Bookauthor = ba,
+                                   Bookpage = bpge,
+                                   Bookprice = bpz
+                                   )
+    return render(request, "bookapp/show.html")
